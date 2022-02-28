a896aed28fd755ad4cfceb8676d68935d7dea886
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-28 12:04:02 -0500

    styling files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+import './App.css';
+
+const apiKey = 'djnmxeetfwkc';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Medical Pager & Chat Application</h1>
+    <div className= "app-wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+            
+            />
+            <ChannelContainer
+            
+            />
+        </Chat>
     </div>
   );
 }
 
-export default App
+export default App;
